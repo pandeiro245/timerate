588c69fed373a357b8b7acb82436ce02c48db347
Nishikokura Hironobu <nishiko@mindia.jp>
2019-12-28 04:52:33 +0000

    add Person

---
+++ db/migrate/20191228044831_create_people.rb
@@ -0,0 +1,10 @@
+class CreatePeople < ActiveRecord::Migration[6.0]
+  def change
+    create_table :people do |t|
+      t.belongs_to :user, foreign_key: true
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_28_043244) do
+ActiveRecord::Schema.define(version: 2019_12_28_044831) do
+
+  create_table "people", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_people_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,4 +27,5 @@ ActiveRecord::Schema.define(version: 2019_12_28_043244) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "people", "users"
 end
