84907b8a28b92faad78ace3af388f47cf72d88a9
Nishikokura Hironobu <nishiko@mindia.jp>
2019-12-28 05:55:21 +0000

    update events#index

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,10 @@
 class EventsController < ApplicationController
   def index
-    @events = Event.all
+    current_user = nil
+    if current_user.present?
+      @events = Event.where(user: current_user)
+    else
+      render html: 'this service is invaited only.'
+    end
   end
 end
